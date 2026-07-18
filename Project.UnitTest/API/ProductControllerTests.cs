c8a91ca0b75d81cd6c8c99768ab569136c1d0267
Matthew Camilleri <79280069+Kryptic869@users.noreply.github.com>
2026-07-18 12:26:52 +0200

    Update ProductControllerTests.cs
    Changes to Tests inorder for build to work.

--- Project.UnitTest/API/ProductControllerTests.cs
+++ Project.UnitTest/API/ProductControllerTests.cs
@@ -3,49 +3,95 @@ using Project.Core.Entities.Business;
 using Project.Core.Interfaces.IServices;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;
 using Moq;
 
 namespace Project.UnitTest.API
 {
     public class ProductControllerTests
     {
-        private Mock<IProductService> _productServiceMock;
-        private Mock<ILogger<ProductController>> _loggerMock;
-        private ProductController _productController;
+        private Mock<IProductService> _productServiceMock = null!;
+        private Mock<ILogger<ProductController>> _loggerMock = null!;
+        private MemoryCache _memoryCache = null!;
+        private ProductController _productController = null!;
 
         [SetUp]
         public void Setup()
         {
             _productServiceMock = new Mock<IProductService>();
             _loggerMock = new Mock<ILogger<ProductController>>();
-            _productController = new ProductController(_loggerMock.Object, _productServiceMock.Object);
+            _memoryCache = new MemoryCache(
+                new MemoryCacheOptions()
+            );
+            _productController = new ProductController(_loggerMock.Object, _productServiceMock.Object, _memoryCache);
         }
 
         [Test]
-        public async Task Get_ReturnsViewWithListOfProducts()
+        public async Task Get_ReturnsOkWithListOfProducts()
         {
             // Arrange
             var products = new List<ProductViewModel>
             {
-                new ProductViewModel { Id = 1, Code = "P001", Name = "Product A", Price = 9.99f, IsActive = true },
-                new ProductViewModel { Id = 2, Code = "P002", Name = "Product B", Price = 19.99f, IsActive = true }
+                new()
+                { 
+                    Id = 1, 
+                    Code = "P001", 
+                    Name = "Product A", 
+                    Price = 9.99f, 
+                    IsActive = true 
+                },
+                new()
+                { 
+                    Id = 2, 
+                    Code = "P002", 
+                    Name = "Product B", 
+                    Price = 19.99f, 
+                    IsActive = true 
+                }
             };
 
-            _productServiceMock.Setup(service => service.GetAll(It.IsAny<CancellationToken>()))
+            _productServiceMock.Setup(service => service.GetAll(CancellationToken.None))
                       .ReturnsAsync(products);
 
             // Act
-            var result = await _productController.Get(It.IsAny<CancellationToken>());
+            IActionResult result = await _productController.Get(CancellationToken.None);
 
             // Assert
-            Assert.IsInstanceOf<OkObjectResult>(result);
-            var okObjectResult = (OkObjectResult)result;
-            Assert.NotNull(okObjectResult);
+            var okResult = result as OkObjectResult;
 
-            var model = (IEnumerable<ProductViewModel>)okObjectResult.Value;
-            Assert.NotNull(model);
-            Assert.That(model.Count(), Is.EqualTo(products.Count));
+            Assert.That(okResult, Is.Not.Null);
 
+            var response = okResult!.Value as ResponseViewModel<IEnumerable<ProductViewModel>>;
+
+            Assert.That(response, Is.Not.Null);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response!.Success, Is.True);
+
+                Assert.That(
+                    response.Message,
+                    Is.EqualTo("Products retrieved successfully")
+                );
+
+                Assert.That(response.Data, Is.Not.Null);
+
+                Assert.That(
+                    response.Data!.Count(),
+                    Is.EqualTo(products.Count)
+                );
+
+                Assert.That(
+                    response.Data.Select(products => products.Id),
+                    Is.EqualTo(products.Select(product => product.Id))
+                );
+            });
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _memoryCache.Dispose();
         }
 
         // Add more test methods for other controller actions, such as Create, Update, Delete, etc.
